71778097def7e95f8c241707193514f182593b81
Jenn-Torres <jennifer.rosalia.gibert@gmail.com>
2023-03-31 10:46:43 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.shecodes.io/graduates/57892-jennifer-torres">
-          Jennifer Torres{" "}
-        </a>{" "}
-        and is open sourced on{" "}
-        <a
-          href="https://github.com/Jenn-Torres/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.shecodes.io/graduates/57892-jennifer-torres">
+            Jennifer Torres{" "}
+          </a>{" "}
+          and is open sourced on{" "}
+          <a
+            href="https://github.com/Jenn-Torres/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
