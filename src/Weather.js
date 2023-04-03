2816157935e5cbba6eac7c30a5b69dd0e1dbb7b9
Jenn-Torres <jennifer.rosalia.gibert@gmail.com>
2023-04-03 22:30:54 +0100

    FormattedDate

--- src/Weather.js
+++ src/Weather.js
@@ -1,82 +1,97 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  let [city, setCity] = useState("");
-  let [weatherData, setWeatherData] = useState({});
+export default function Weather(props) {
+  let [city, setCity] = useState(props.defaultCity);
+  let [weatherData, setWeatherData] = useState({ ready: false });
 
-  function search(event) {
-    event.preventDefault();
+  function search() {
+    console.log("city");
     const apiKey = "a33b693cfbefd271b0ed075f9a8f65f0";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(url).then(handleResponse);
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleResponse(response) {
     setWeatherData({
+      ready: true,
       temperature: response.data.main.temp,
+      date: new Date(response.data.dt * 1000),
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon,
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function updateCity(event) {
     setCity(event.target.value);
   }
 
-  return (
-    <div className="Weather">
-      <form onSubmit={search}>
-        <div class="row">
-          <div class="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              class="form-control"
-              onChange={updateCity}
-            />
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                onChange={updateCity}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+                autoFocus="on"
+                autoComplete="off"
+              />
+            </div>
           </div>
-          <div class="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-              autofocus="on"
-              autocomplete="off"
-            />
-          </div>
-        </div>
-      </form>
-      <h1 className="text-capitalize">{weatherData.city}</h1>
-      <ul>
-        <li>{weatherData.description}</li>
-      </ul>
-      <div className="row mt-4">
-        <div className="col-6">
-          <div className="d-flex">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Mostly Cloudy"
-              className="float-left"
-            />
+        </form>
+        <h1 className="text-capitalize">{weatherData.city}</h1>
+        <ul>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-4">
+          <div className="col-6">
+            <div className="d-flex">
+              <img
+                src={weatherData.iconUrl}
+                alt="Mostly Cloudy"
+                className="float-left"
+              />
 
-            <span className="temperature">
-              {Math.round(weatherData.temperature)}
-            </span>
-            <span className="unit">°C</span>
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {Math.round(weatherData.wind)} Km/h</li>
-          </ul>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {Math.round(weatherData.wind)} Km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+  }
 }
